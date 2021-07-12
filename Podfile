741c390470b09658e13992572b4ae8c1b4574816
James Choi <>
2021-07-12 17:55:59 +0900

    made watch

---
+++ Podfile
@@ -0,0 +1,37 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'first_build_watch' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for first_build_watch
+
+  pod 'Gifu'
+
+  target 'first_build_watchTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'first_build_watchUITests' do
+    # Pods for testing
+  end
+
+end
+
+target 'first_build_watch WatchKit App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for first_build_watch WatchKit App
+
+end
+
+target 'first_build_watch WatchKit Extension' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for first_build_watch WatchKit Extension
+
+end
--- first_build_watch WatchKit Extension/ContentView.swift
+++ first_build_watch WatchKit Extension/ContentView.swift
@@ -8,10 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTime = Time(sec: 0, min: 0, hr: 0)
+    @State var receiver = Timer.publish(every: 1, on: .current, in: .default).autoconnect()
+    
+    var width = WKInterfaceDevice.current().screenBounds.size.width
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        ZStack{
+            // Main clock
+            Circle()
+                .fill(Color(.blue).opacity(0.1))
+            
+            // Seconds and Min dots
+            
+//            ForEach(0..<60, id: \.self) {i in
+//                Rectangle()
+//                    .fill(Color(.green))
+//                    .frame(width: 2, height: (i % 5) == 0 ? 10 : 5)
+//                    .offset(y: (width - 40) / 2)
+//                    .rotationEffect(.init(degrees: Double(i * 6)))
+//            }
+            
+            // Second hand
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 2, height: (width - 60) / 2)
+                .offset(y: -(width - 60) / 4)
+                .rotationEffect(.init(degrees: Double(currentTime.sec) * 6))
+            
+            // Minute hand
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 2, height: (width - 90) / 2)
+                .offset(y: -(width - 90) / 4)
+                .rotationEffect(.init(degrees: Double(currentTime.min) * 6 + Double(currentTime.sec) * 0.1))
+            
+            // Hour hand
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 2, height: (width - 120) / 2)
+                .offset(y: -(width - 120) / 4)
+                .rotationEffect(.init(degrees: Double(currentTime.hr) * 30 + Double(currentTime.min) * 0.5 + Double(currentTime.sec) * 0.5/60))
+            
+            // Center Circle
+            Circle()
+                .fill(Color.red)
+                .frame(width:8, height: 8)
+        }
+        .onReceive(receiver, perform: { _ in
+            let calendar = Calendar.current
+            
+            let sec = calendar.component(.second, from: Date())
+            let min = calendar.component(.minute, from: Date())
+            let hr = calendar.component(.hour, from: Date())
+            
+            withAnimation(Animation.linear(duration: 0.01)) {
+                self.currentTime = Time(sec: sec, min: min, hr: hr)
+            }
+        })
+        
+        
     }
+}
+
+struct Time {
+    var sec: Int
+    var min: Int
+    var hr: Int
 }
 
 struct ContentView_Previews: PreviewProvider {
